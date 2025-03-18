7a29353efa48c34e7b08d0fb8e07ee3c15641b95
vlalana <vlalana@xtec.cat>
2025-03-18 11:08:02 +0100

    Fitxers inicials per al projecte MakeCode

---
+++ main.ts
@@ -0,0 +1,59 @@
+let nombre = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    nombre = randint(0, 6)
+    if (nombre == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (nombre == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (nombre == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    }
+    if (nombre == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (nombre == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (nombre == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
