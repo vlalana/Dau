47f77586a7001aa75ae4b0aad489193caf5d481b
vlalana <vlalana@xtec.cat>
2025-03-18 15:34:53 +0100

    Actualització pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,59 +1,55 @@
-let nombre = 0
-input.onGesture(Gesture.Shake, function () {
-    basic.clearScreen()
+nombre = 0
+
+def on_gesture_shake():
+    global nombre
+    basic.clear_screen()
     nombre = randint(0, 6)
-    if (nombre == 1) {
-        basic.showLeds(`
+    if nombre == 1:
+        basic.show_leds("""
             . . . . .
             . . . . .
             . . # . .
             . . . . .
             . . . . .
-            `)
-    }
-    if (nombre == 2) {
-        basic.showLeds(`
+            """)
+    if nombre == 2:
+        basic.show_leds("""
             . . . . .
-            . # . . .
             . . . . .
-            . . . # .
+            . # . # .
             . . . . .
-            `)
-    }
-    if (nombre == 3) {
-        basic.showLeds(`
+            . . . . .
+            """)
+    if nombre == 3:
+        basic.show_leds("""
             . . . . .
             . # . . .
             . . # . .
             . . . # .
             . . . . .
-            `)
-    }
-    if (nombre == 4) {
-        basic.showLeds(`
+            """)
+    if nombre == 4:
+        basic.show_leds("""
             . . . . .
             . # . # .
             . . . . .
             . # . # .
             . . . . .
-            `)
-    }
-    if (nombre == 5) {
-        basic.showLeds(`
+            """)
+    if nombre == 5:
+        basic.show_leds("""
             . . . . .
             . # . # .
             . . # . .
             . # . # .
             . . . . .
-            `)
-    }
-    if (nombre == 6) {
-        basic.showLeds(`
+            """)
+    if nombre == 6:
+        basic.show_leds("""
             . . . . .
             . # . # .
             . # . # .
             . # . # .
             . . . . .
-            `)
-    }
-})
+            """)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
